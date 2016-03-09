ca35e564e78366509633a7c45169c9d70ca41a4e
Daniil Tarakanov <daniil@tarakanov.me>
2016-03-09 16:29:21 +0000

    Added connection view.

--- GymBuddy/ViewController.swift
+++ GymBuddy/ViewController.swift
@@ -11,15 +11,34 @@ import GLKit
 
 class ViewController: UIViewController {
 
+    @IBAction func connectButton(sender: AnyObject) {
+        let controller = TLMSettingsViewController.settingsInNavigationController()
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let notifer = NSNotificationCenter.defaultCenter()
+        // Data notifications are received through NSNotificationCenter.
+        notifer.addObserver(self, selector: "didConnectDevice:", name: TLMHubDidConnectDeviceNotification, object: nil)
+        notifer.addObserver(self, selector: "didDisconnectDevice:", name: TLMHubDidDisconnectDeviceNotification, object: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func didConnectDevice(notification: NSNotification) {
+        // Show a list of exercises
+        print("Connected")
+    }
+    
+    func didDisconnectDevice(notification: NSNotification) {
+        print("disconnected =(")
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        let controller = TLMSettingsViewController.settingsInNavigationController()
-        presentViewController(controller, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
