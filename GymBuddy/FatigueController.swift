f08375861df44bd8baab941f4d711d2bee27cb08
Daniil Tarakanov <daniil@tarakanov.me>
2016-03-10 19:58:27 +0000

    Getting EMG, Accelerometer and Sync/Unsync data from Myo!

--- GymBuddy/FatigueController.swift
+++ GymBuddy/FatigueController.swift
@@ -7,11 +7,41 @@
 //
 
 import Foundation
+import GLKit
 
 class FatigueController {
     
     init()
     {
         status.fatigue.value = 3
+        let notifer = NSNotificationCenter.defaultCenter()
+        // Posted whenever the user does a Sync Gesture, and the Myo is calibrated
+        notifer.addObserver(self, selector: "didRecognizeArm:", name: TLMMyoDidReceiveArmSyncEventNotification, object: nil)
+        // Posted whenever Myo loses its calibration (when Myo is taken off, or moved enough on the user's arm)
+        notifer.addObserver(self, selector: "didLoseArm:", name: TLMMyoDidReceiveArmUnsyncEventNotification, object: nil)
+        // Notifications for EMG event are posted at a rate of 50 Hz.
+        notifer.addObserver(self, selector: "didReceiveEmgEvent:", name: TLMMyoDidReceiveEmgEventNotification, object: nil)
+    }
+    
+    func didRecognizeArm(notification: NSNotification) {
+        let eventData = notification.userInfo as! Dictionary<NSString, TLMArmSyncEvent>
+        let armEvent = eventData[kTLMKeyArmSyncEvent]!
+        
+        var arm = armEvent.arm == .Right ? "Right" : "Left"
+        var direction = armEvent.xDirection == .TowardWrist ? "Towards Wrist" : "Toward Elbow"
+        print("SYNCED: Arm: \(arm) X-Direction: \(direction)")
+        
+        armEvent.myo!.vibrateWithLength(.Short)
+    }
+    
+    func didLoseArm(notification: NSNotification) {
+        print("Perform the Sync Gesture")
+    }
+    
+    func didReceiveEmgEvent(notification: NSNotification) {
+        let eventData = notification.userInfo as! Dictionary<NSString, TLMEmgEvent>
+        let emgEvent = eventData[kTLMKeyEMGEvent]!
+        
+        let emg = 1
     }
 }
--- GymBuddy/StatusViewController.swift
+++ GymBuddy/StatusViewController.swift
@@ -13,6 +13,8 @@ import Bond
 class StatusViewController: UIViewController {
     
     var timer = 0
+    var currentPose: TLMPose!
+    var currentEmg: TLMEmgEvent!
     
     @IBOutlet weak var fatigueLabel: UILabel!
     @IBOutlet weak var repsLabel: UILabel!
@@ -43,9 +45,63 @@ class StatusViewController: UIViewController {
         
         // Start the timer
         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "timerDidFire", userInfo: nil, repeats: true)
+        
+        //TLMMyo.setStreamEmg(.Enabled)
+        
+        // Myo notifications
+        let notifer = NSNotificationCenter.defaultCenter()
+        notifer.addObserver(self, selector: "didDisconnectDevice:", name: TLMHubDidDisconnectDeviceNotification, object: nil)
+        // Posted whenever the user does a Sync Gesture, and the Myo is calibrated
+        notifer.addObserver(self, selector: "didRecognizeArm:", name: TLMMyoDidReceiveArmSyncEventNotification, object: nil)
+        // Posted whenever Myo loses its calibration (when Myo is taken off, or moved enough on the user's arm)
+        notifer.addObserver(self, selector: "didLoseArm:", name: TLMMyoDidReceiveArmUnsyncEventNotification, object: nil)
+        // Notifications for EMG event are posted at a rate of 50 Hz.
+        notifer.addObserver(self, selector: "didRecieveEmgEvent:", name: TLMMyoDidReceiveEmgEventNotification, object: nil)
+        // Notifications accelerometer event are posted at a rate of 50 Hz.
+        notifer.addObserver(self, selector: "didRecieveAccelerationEvent:", name: TLMMyoDidReceiveAccelerometerEventNotification, object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
+    }
+    
+    func didDisconnectDevice(notification: NSNotification) {
+        print("disconnected =(")
+    }
+    
+    func didRecognizeArm(notification: NSNotification) {
+        let eventData = notification.userInfo as! Dictionary<NSString, TLMArmSyncEvent>
+        let armEvent = eventData[kTLMKeyArmSyncEvent]!
+        
+        var arm = armEvent.arm == .Right ? "Right" : "Left"
+        var direction = armEvent.xDirection == .TowardWrist ? "Towards Wrist" : "Toward Elbow"
+        print("SYNCED: Arm: \(arm) X-Direction: \(direction)")
+        
+        armEvent.myo!.vibrateWithLength(.Short)
+        armEvent.myo!.setStreamEmg(.Enabled)
+    }
+    
+    func didLoseArm(notification: NSNotification) {
+        print("Perform the Sync Gesture")
+    }
+    
+    func didRecieveEmgEvent(notification: NSNotification) {
+        let eventData = notification.userInfo as! Dictionary<NSString, TLMEmgEvent>
+        let emgEvent = eventData[kTLMKeyEMGEvent]!
+        
+        let emg = 1
+    }
+    
+    func didRecieveAccelerationEvent(notification: NSNotification) {
+        let eventData = notification.userInfo as! Dictionary<NSString, TLMAccelerometerEvent>
+        let accelerometerEvent = eventData[kTLMKeyAccelerometerEvent]!
+        
+        let acceleration = GLKitPolyfill.getAcceleration(accelerometerEvent);
+        print("\(acceleration.magnitude)")
+        
+        // Uncomment to show direction of acceleration
+        print("x: \(acceleration.x)")
+        print("y: \(acceleration.y)")
+        print("z: \(acceleration.z)")
     }
     
     func timerDidFire()
