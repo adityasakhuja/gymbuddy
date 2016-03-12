a95a7f964baeeab0b905a1764c99f5f3478252c3
g2384 <g2384@users.noreply.github.com>
2016-03-12 18:51:52 +0000

    Update RestController

--- GymBuddy/RestController.swift
+++ GymBuddy/RestController.swift
@@ -14,44 +14,109 @@ class RestController: NSObject {
     {
         super.init()
         
-        calculateRestingTime()
         calculateNextReps()
-        calculateNextWeight()
-    }
-    
-    func calculateRestingTime()
-    {
-        // Get previous fatigues array [Int]
-        let fatigues = fatigueGlobal
-        
-        // Do magic
-        
-        // Return resting time in seconds
-        rest.time.value = 240
-        
-        // Flush the global array when you are done
-        fatigueGlobal = []
     }
     
     func calculateNextReps()
     {
-        // Get previous reps
-        let repsPrev = status.reps.value
+        print("input: fatigues: \(fatigues),\t reps: \(repsPrev),\t Weight: \(weightPrev),\t rest: \(restPrev)")
         
-        // Do magic
+        let fatigues = fatigueGlobal// Get previous fatigues array [Int]
+        let repsPrev = status.reps.value// Get previous reps
+        let weightPrev = status.weight.value// Get previous weight
+        let restPrev = rest.time.value
         
-        // Reset the status reps
-        status.reps.value = 0
-    }
-    
-    func calculateNextWeight()
-    {
-        // Get previous weight
-        let weightPrev = status.weight.value
+        let fatigue_curr = fatigues[fatigues.endIndex - 1]
+        let fatigue_init = fatigues[0]
         
-        // Do magic
+        //CONSTANTS
+        let weight_threshold = 10
+        let tiredness_threshold = 3
+        let max_rest_time = 300
         
-        // Set the new weight
-        status.weight.value = 3
+        var rest_time = restPrev
+        var status_weight = weightPrev
+        var status_rep = repsPrev
+        
+        switch fatigue_curr {
+            case 1:
+                if(repsPrev >= 8){
+                    if(status_weight > weight_threshold){
+                        status_weight += 1
+                    }else{
+                        status_weight += 2
+                    }
+                    status_rep = 8
+                } else {
+                    status_rep += 3
+                }
+                rest_time = 30 // should start immediately
+            case 2:
+                if(repsPrev >= 8){
+                    status_weight += 1
+                    status_rep = 8
+                } else {
+                    status_rep += 2
+                }
+                rest_time = 30 // should start immediately
+            case 3:
+                if(fatigue_curr == tiredness_threshold){
+                    rest_time += 30
+                    if(rest_time >= max_rest_time){
+                        rest_time = max_rest_time
+                        if(repsPrev > 8){
+                            status_rep = 8
+                        }else{
+                            status_rep -= 1
+                        }
+                    }
+                }
+                
+            case 4:
+                if(restPrev >= max_rest_time){
+                    status_weight -= 1
+                }else {
+                    if (repsPrev > 8){
+                        status_rep = 8
+                    }
+                    if(fatigue_init >= tiredness_threshold ){
+                        rest_time += 30
+                    } else if(fatigue_init == 1){
+                        rest_time -= 30
+                    }
+                }
+            case 5:
+                if(restPrev >= max_rest_time){
+                    status_weight -= 1
+                    status_rep = 8
+                    if(status_weight > weight_threshold){
+                        if(fatigue_init < tiredness_threshold){
+                            status_weight -= 1
+                        }
+                    }
+                }else {
+                    if (repsPrev > 8){
+                        status_rep = 8
+                        rest_time += 30
+                    }else{
+                        if(fatigue_init < tiredness_threshold){
+                            status_rep -= 1
+                        }else{
+                            status_rep -= 1
+                            rest_time += 30
+                        }
+                    }
+                }
+            default:
+                // do nothing
+                print("default")
+        }
+        
+        print("rest time: \(rest_time),\t weight: \(status_weight),\t reps: \(status_rep) \n")
+        
+        rest.time.value = rest_time// Return resting time in seconds
+        status.weight.value = status_weight// Set the new weight
+        status.reps.value = status_rep// Reset the status reps
+        fatigueGlobal = []// Flush the global array when you are done
     }
 }
